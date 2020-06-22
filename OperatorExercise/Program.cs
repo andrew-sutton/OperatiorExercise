84ec1e96c1c5be729b66a432d92043b3e1d531f2
Andrew Sutton <ajsutton94@gmail.com>
2020-06-22 11:08:34 -0500

    first commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input A:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please input B:");
+            int b = int.Parse(Console.ReadLine());
+
+            Addition(a, b);
+
+            Subtraction(a, b);
+
+            Multiplication(a, b);
+
+            Division(a, b);
+
+
+
+        }
+        public static void Addition(int a, int b)
+        {
+            var result = a + b;
+            Console.WriteLine($"{a}+{b} is {result}");
+        }
+        public static void Subtraction(int a, int b)
+        {
+            var result = a - b;
+            Console.WriteLine($"{a}-{b} is {result}");
+        }
+        public static void Multiplication(int a, int b)
+        {
+            var result = a * b;
+            Console.WriteLine($"{a}*{b} is {result}");
+        }
+        public static void Division(int a, int b)
+        {
+            var result = a / b;
+            var quotient = a % b;
+
+            Console.WriteLine($"{a}/{b} is {result} remainder {quotient}");
+        }
+    }
+}
